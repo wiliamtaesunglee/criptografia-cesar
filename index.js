8a1cdae8140d0f2e82254373fd7d303795125d3d
wim <wiliam.tae@gmail.com>
2020-01-20 09:27:12 -0200

    answare text updated

--- index.js
+++ index.js
@@ -2,8 +2,11 @@ const fetch = require('node-fetch');
 const fs = require('fs');
 const sha1 = require('js-sha1');
 const FormData = require('form-data');
+const file = require('./answer.json');
 
 const form = new FormData();
+
+form.append('myFile', file)
 
 const token = '512fc2dbee90b61b4b5a05015b3f19d56b4b6a42'
 
@@ -35,7 +38,8 @@ fetch(`https://api.codenation.dev/v1/challenge/dev-ps/generate-data?token=${toke
       };
 
       let newResponse = response.join('');
-      data.decifrado = sha1(newResponse);
+      data.decifrado = newResponse
+      data.resumo_criptografico = sha1(newResponse)
       console.log(data);
 
       //Create a new file
@@ -44,35 +48,6 @@ fetch(`https://api.codenation.dev/v1/challenge/dev-ps/generate-data?token=${toke
        err ? console.log(err) : console.log('saved');
       });
 
-
-
-      /*async function postData(url = `https://api.codenation.dev/v1/challenge/dev-ps/submit-solution?token=${token}`, data = {}) {
-      // Default options are marked with *
-        const response = await fetch(url, {
-            method: 'POST', // *GET, POST, PUT, DELETE, etc.
-            mode: 'cors', // no-cors, *cors, same-origin
-            cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-            credentials: 'same-origin', // include, *same-origin, omit
-            headers: {
-              'Content-Type': 'application/json'
-              // 'Content-Type': 'application/x-www-form-urlencoded',
-            },
-            redirect: 'follow', // manual, *follow, error
-            referrerPolicy: 'no-referrer', // no-referrer, *client
-            body: JSON.stringify(data) // body data type must match "Content-Type" header
-          });
-          return await response.json(); // parses JSON response into native JavaScript objects
-          }*/
-
-      //post data
-
-
-
-
-
-      //post data
-
-
       });
     }
   )
@@ -80,3 +55,30 @@ fetch(`https://api.codenation.dev/v1/challenge/dev-ps/generate-data?token=${toke
     console.log('Fetch Error :-S', err);
   });
 
+
+
+      async function postData(url , data) {
+      // Default options are marked with *
+        const response = await fetch(url, {
+            method: 'POST', // *GET, POST, PUT, DELETE, etc.
+            mode: 'cors', // no-cors, *cors, same-origin
+            cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+            credentials: 'same-origin', // include, *same-origin, omit
+            headers: {
+              'Content-Type': 'multipart/form-data'
+              // 'Content-Type': 'application/x-www-form-urlencoded',
+            },
+            redirect: 'follow', // manual, *follow, error
+            referrerPolicy: 'no-referrer', // no-referrer, *client
+            body: data // body data type must match "Content-Type" header
+          });
+          return await response.json(); // parses JSON response into native JavaScript objects
+          }
+
+      postData(`https://api.codenation.dev/v1/challenge/dev-ps/submit-solution?token=${token}`, form)
+        .then((data) => {
+          console.log(data)
+        })
+        .catch((err) => {
+          console.log(err)
+        })
